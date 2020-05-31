9b189db30b135be0caead637290ecff8d181709b
Pat <p171982@gmail.com>
2020-05-31 20:37:31 +0100

    simply layout

---
+++ footerwalbrzych.php
@@ -0,0 +1,15 @@
+<?php
+echo '
+     <!--Footer-->
+  <div class="container-fluid" id="footer">
+    <div class="row">
+      <div class="col-md-5" id="kontakt0">WIRAZ jazda uzależnia...</div>
+      <div class="col-md-3" id="kontakt1"> Kontakt: 691 806 913</div>
+      <div class="col-md-4" id="kontakt2">Przekonaj sie,ze mamy wszystko</br>co potrzebujesz,aby mieć prawo jazdy</br>Zapraszamy
+        na kursy.</div>
+    </div>
+  </div>
+
+';
+
+?>
---
+++ kategoriamotocykle.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Nauka jazdy motory Wałbrzych.Kategoria A, A2, AM , A1 w Wałbrzychu.</title>
+        <meta name="robots" content="index,fallow">
+        <meta charset="UTF-8">
+        <meta name="description" content="Nauka jazdy kategoria A , A2, AM ,A1 w Wałbrzychu. Kursy Prawa Jazdy na motory.">
+        <meta name="keywords" content="nauka jazdy motory Wałbrzych , prawo jazdy, kategoria A A2 AM A1, 
+        Wałbrzych, egzamin kategoria A Wałbrzych, nauka jazdy motory, nauka jazdy motory Wałbrzych, kursy motocyklowe, kategoria A, kategoria prawo jazdy A2, kategoria A1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap4 -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="./stylingwalbrzych/motorywalbrzych.css">
+        
+    </head>
+    <body>
+    <?php include '../header4_bikes.php' ?>
+        <div class="container">
+            <div class="row" id="row1">
+                <div class="col-sm-12 col-md-6">
+                    <h1>Nauka Jazdy Motorami Wałbrzych</h1>
+                    <h5 style="margin-top:10%;">kategorie prawa jazdy:</h5><br>
+                    <h3>kategorie A, A2, AM , A1</h3>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                <p style="font-size:36px; font-wieght: bold">wrzuć bieg , poczuj pasję</p>
+                <p><span style="font-size:25px; font-wieght: bold">wysoka zdawalność</span>
+                | <span style="font-weight:bold">czołowe lokaty w regionie</span>
+                </p>
+                <p><span style="font-size:25px; font-wieght: bold">maszyny egzaminacyjne</span><br>
+                | <span style="font-weight:bold">Yamaha MT07 | XJ06 | YBR125 | Romet ZXT50</span>
+                </p>
+                </div>
+            </div>
+            <div class="row" id="row2">
+                <div class="col-sm-12 col-md-3" id="karta">
+                    <div class="row">
+                        <div class="col-12">
+                        <h5>kategoria A</h5>
+                        </div>
+                        <div class="col-12" id="body" style="background-image: url('./stylingwalbrzych/img/fot_a.jpg');">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-3" id="karta">
+                <h5>kategoria A2</h5>
+                    <div id="body">pierwsze foto</div>
+                </div>
+                <div class="col-sm-12 col-md-3" id="karta">
+                <h5>kategoria A1</h5>
+                    <div id="body">pierwsze foto</div>
+                </div>
+                <div class="col-sm-12 col-md-3" id="karta">
+                <h5>kategoria AM</h5>
+                    <div id="body">pierwsze foto</div>
+                </div>
+            </div>
+        </div>
+<!-- Footer -->
+
+<!-- Bootstrap Tags  -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<!-- END of Bootstrap Tags  -->
+    </body>
+</html>
