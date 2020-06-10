46d599e7a739a12e57451cfbe398c98e9103b288
Pat <p171982@gmail.com>
2020-06-10 21:43:04 +0100

    some new

--- kategoriamotocykle.php
+++ kategoriamotocykle.php
@@ -1,85 +1,89 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Nauka jazdy motory Wałbrzych.Kategoria A, A2, AM , A1 w Wałbrzychu.</title>
-        <meta name="robots" content="index,fallow">
-        <meta charset="UTF-8">
-        <meta name="description" content="Nauka jazdy kategoria A , A2, AM ,A1 w Wałbrzychu. Kursy Prawa Jazdy na motory.">
-        <meta name="keywords" content="nauka jazdy motory Wałbrzych , prawo jazdy, kategoria A A2 AM A1, 
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Nauka jazdy motory Wałbrzych.Kategoria A, A2, AM , A1 w Wałbrzychu.</title>
+    <meta name="robots" content="index,fallow">
+    <meta charset="UTF-8">
+    <meta name="description" content="Nauka jazdy kategoria A , A2, AM ,A1 w Wałbrzychu. Kursy Prawa Jazdy na motory  Wałbrzych.">
+    <meta name="keywords" content="nauka jazdy motory Wałbrzych , prawo jazdy, kategoria A A2 AM A1, 
         Wałbrzych, egzamin kategoria A Wałbrzych, nauka jazdy motory, nauka jazdy motory Wałbrzych, kursy motocyklowe, kategoria A, kategoria prawo jazdy A2, kategoria A1">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Bootstrap4 -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link rel="stylesheet" href="./stylingwalbrzych/motorywalbrzych.css">
-        
-    </head>
-    <body>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap4 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="./stylingwalbrzych/motorywalbrzych.css">
+
+</head>
+
+<body>
     <?php include '../header4_bikes.php' ?>
-        <div class="container">
-            <div class="row" id="row1">
-                <div class="col-sm-12 col-md-6">
-                    <h1>Nauka Jazdy Motorami Wałbrzych</h1>
-                    <h5 style="margin-top:10%;">kategorie prawa jazdy:</h5><br>
-                    <h3>kategorie A, A2, AM , A1</h3>
-                </div>
-                <div class="col-sm-12 col-md-6">
+    <div class="container">
+        <div class="row" id="row1">
+            <div class="col-sm-12 col-md-6">
+                <h1>Nauka Jazdy Motorami Wałbrzych <span style="color:red;font-size:45px;font-weight:bold">Wiraż</span></h1>
+                <h5 style="margin-top:10%;">motocyklowe prawa jazdy:</h5><br>
+                <h3>Wałbrzych kategorie A, A2, AM , A1</h3>
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <br><br>
                 <p><span style="font-size:25px;">wysoka zdawalność</span>
-                | <span style="font-weight:bold">czołowe lokaty w regionie</span>
+                    | <span style="font-weight:bold">czołowe lokaty w regionie</span>
                 </p>
                 <p><span style="font-size:25px;">maszyny egzaminacyjne</span><br>
-                | <span style="font-weight:bold">Yamaha MT07 | XJ06 | YBR125 | Romet ZXT50</span>
+                    | <span style="font-weight:bold">Yamaha MT07 | XJ06 | YBR125 | Romet ZXT50</span>
                 </p>
                 <br>
                 <p style="font-size:36px; font-weight: bold; color:rgb(252, 5, 26)">wrzuć bieg , poczuj pasję</p>
-                </div>
             </div>
-            <div class="row" id="row2">
-                <div class="col-sm-12 col-md-3" id="karta">
-                    <a href="../nauka_jazdy_motor/kategoria_A.php">
+        </div>
+        <div class="row" id="row2">
+            <div class="col-sm-12 col-md-3" id="karta">
+                <a href="../nauka_jazdy_motor/kategoria_A.php">
                     <h5>kategoria A</h5>
                     <p style="font-size:14px;">MOTOCYKLE W KLASIE POWYŻEJ 600cm</p>
-                        <div id="body">
-                        <img src="./stylingwalbrzych/img/fot_a.jpg" alt="naukajazdy_motor_walbrzych">   
+                    <div id="body">
+                        <img src="./stylingwalbrzych/img/fot_a.jpg" alt="naukajazdy_motor_walbrzych">
                     </div>
-                    </a>
-                </div>
-                
-                <div class="col-sm-12 col-md-3" id="karta">
-                    <a href="../nauka_jazdy_motor/kategoria_A2.php">
+                </a>
+            </div>
+
+            <div class="col-sm-12 col-md-3" id="karta">
+                <a href="../nauka_jazdy_motor/kategoria_A2.php">
                     <h5>kategoria A2</h5>
                     <p style="font-size:14px;">MOTOCYKLE O MOCY DO 35kW.</p>
-                        <div id="body">
-                        <img src="./stylingwalbrzych/img/fot5.jpg" alt="naukajazdy_kategoriaA_A2_A1_walbrzych">   
+                    <div id="body">
+                        <img src="./stylingwalbrzych/img/fot5.jpg" alt="naukajazdy_kategoriaA_A2_A1_walbrzych">
                     </div>
-                    </a>
-                </div>
-                 <div class="col-sm-12 col-md-3" id="karta">
-                    <a href="../nauka_jazdy_motor/kategoria_A2.php">
-                        <h5>kategoria A1</h5>
-                            <p style="font-size:14px;">MOTOCYKL DLA OSÓB W WIEKU 16lat.</p>
-                        <div id="body">
-                            <img src="./stylingwalbrzych/img/katA1.jpg" alt="naukajazdy_kategoriaA_A2_A1_walbrzych">
-                        </div>
-                    </a>
-                </div>
-                <div class="col-sm-12 col-md-3" id="karta">
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-3" id="karta">
+                <a href="../nauka_jazdy_motor/kategoria_A1.php">
+                    <h5>kategoria A1</h5>
+                    <p style="font-size:14px;">MOTOCYKL DLA OSÓB W WIEKU 16lat.</p>
+                    <div id="body">
+                        <img src="./stylingwalbrzych/img/katA1.jpg" alt="naukajazdy_kategoriaA_A2_A1_walbrzych">
+                    </div>
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-3" id="karta">
                 <a href="../nauka_jazdy_motor/motorowery_swidnica_kategoriaAM.php">
                     <h5>kategoria AM</h5>
                     <p style="font-size:14px;">PIERWSZE MOTOKILOMETRY</p>
-                        <div id="body">
-                            <img src="./stylingwalbrzych/img/f9.jpeg" alt="naukajazdy_motorower_walbrzych">
-                        </div>
-                    </a>
-                </div>
+                    <div id="body">
+                        <img src="./stylingwalbrzych/img/f9.jpeg" alt="naukajazdy_motorower_walbrzych">
+                    </div>
+                </a>
             </div>
         </div>
-<!-- Footer -->
-<?php include'footerwalbrzych.php'; ?>
-<!-- Bootstrap Tags  -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-<!-- END of Bootstrap Tags  -->
-    </body>
+    </div>
+    <!-- Footer -->
+    <?php include 'footerwalbrzych.php'; ?>
+    <!-- Bootstrap Tags  -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <!-- END of Bootstrap Tags  -->
+</body>
+
 </html>
